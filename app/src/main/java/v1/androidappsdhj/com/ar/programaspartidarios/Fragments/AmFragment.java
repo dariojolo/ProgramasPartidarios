5593c85b7314259964f537a33c9178a8297f842a
Dario <dariojolo@gmail.com>
2017-10-09 00:14:03 -0300

    Implementado ListView

--- app/src/main/java/v1/androidappsdhj/com/ar/programaspartidarios/Fragments/AmFragment.java
+++ app/src/main/java/v1/androidappsdhj/com/ar/programaspartidarios/Fragments/AmFragment.java
@@ -92,15 +92,13 @@ public class AmFragment extends Fragment implements RealmChangeListener<RealmRes
             }
         });*/
 
-        myAdapter = new MyAdapterListView(getContext(), R.layout.list_item_listview,programasR);
-        myAdapter.notifyDataSetChanged();
         //recycler.getRecycledViewPool().clear();
         //Este metodo se puede usar cuando sabemos que el layout del recycler no van a cambiar de tamaño
         //recycler.setHasFixedSize(true);
         //Se le agrega una animacion por defecto
         //recycler.setItemAnimator(new DefaultItemAnimator());
         //recycler.setLayoutManager(layoutManager);
-        //recycler.setAdapter(adapter);
+        //recycler.setAda   pter(adapter);
         myAdapter = new MyAdapterListView(getContext(), R.layout.list_item_listview,programasR);
         myAdapter.notifyDataSetChanged();
         listView.setAdapter(myAdapter);
